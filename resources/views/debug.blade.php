4865175d4469b8be44b47da26582c8b3203a631d
Caen De Silva <caen@desilva.se>
2022-06-06 21:15:16 +0200

    Add debug page

---
+++ resources/views/debug.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Application Debug Screen</title>
+</head>
+<body>
+	<h1>Application Debug Screen</h1>
+	
+	<section>
+		<h2>Server information</h2>
+		<dl>
+			<dt>PHP Version</dt>
+			<dd>{{ PHP_VERSION }}</dd>
+
+			<dt>PHP SAPI</dt>
+			<dd>{{ php_sapi_name() }}</dd>
+
+			<dt>PHP OS</dt>
+			<dd>{{ PHP_OS }}</dd>
+
+			<dt>Laravel Version</dt>
+			<dd>{{ Illuminate\Foundation\Application::VERSION }}</dd>
+
+			<dt>Laravel Application Path</dt>
+			<dd>{{ app_path() }}</dd>
+
+			<dt>Laravel Base Path</dt>
+			<dd>{{ base_path() }}</dd>
+		</dl>
+	</section>
+
+	<section>
+		<h2>Database information</h2>
+		<dl>
+			<dt>Database Driver</dt>
+			<dd>{{ config('database.default') }}</dd>
+
+			<dt>Database Name</dt>
+			<dd>{{ config('database.connections.'.config('database.default').'.database') }}</dd>
+		</dl>
+	</section>
+</body>
+</html>
