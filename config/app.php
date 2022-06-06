a6e452a89e56036d70586d6adfb78dccb38b906b
Caen De Silva <caen@desilva.se>
2022-06-06 22:00:48 +0200

    Automatically migrate the database

--- config/app.php
+++ config/app.php
@@ -197,6 +197,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         App\Providers\CoreServiceProvider::class,
+        App\Providers\MigrationServiceProvider::class,
     ],
 
     /*
