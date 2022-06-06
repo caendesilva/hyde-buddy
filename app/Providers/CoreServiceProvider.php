0726dfc9f539eb05d9475ef703e33fff000b80b0
Caen De Silva <caen@desilva.se>
2022-06-06 21:27:07 +0200

    Bind the home path and ensure critical files exist

--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -7,22 +7,38 @@ use Illuminate\Support\ServiceProvider;
 class CoreServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
+     * Register the core services.
      *
      * @return void
      */
     public function register()
     {
-        //
+        // Register the home path
+        $this->app->bind('homePath', function () {
+            return getenv('HOMEDRIVE') . getenv('HOMEPATH').'\.hyde-buddy';
+        });
     }
 
     /**
-     * Bootstrap services.
+     * Bootstrap the core services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        // Create the home path if it doesn't exist
+        if (! file_exists($this->app->make('homePath'))) {
+            mkdir($this->app->make('homePath'), recursive: true);
+        }
+
+        // Create the database path if it doesn't exist
+        if (! file_exists($this->app->make('homePath') . '\\database')) {
+            mkdir($this->app->make('homePath') . '\\database', recursive: true);
+        }
+
+        // Create the database file if it doesn't exist
+        if (! file_exists($this->app->make('homePath') . '\\database\\database.sqlite')) {
+            touch($this->app->make('homePath') . '\\database\\database.sqlite');
+        }
     }
 }
