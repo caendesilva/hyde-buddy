96c63a959c448906daaff3222638648cc21bff1b
Caen De Silva <caen@desilva.se>
2022-06-06 22:18:26 +0200

    Load the stylesheet

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hyde Buddy {{ isset($title) ? " - $title" : '' }}</title>
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
 </head>
 <body>
     <main>
