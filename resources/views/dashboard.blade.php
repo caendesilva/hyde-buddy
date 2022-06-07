cd7b6bbb3dc7200fa28a5d769e3e4f6cbbe368d6
Caen De Silva <caen@desilva.se>
2022-06-07 13:20:07 +0200

    Add table with base information

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,5 +2,25 @@
     <x-slot name="title">Project Dashboard</x-slot>
     <h1 class="text-center m-large">Project Dashboard</h1>
 
-	
+	<table class="mx-auto">
+		<caption>
+			Project Information
+		</caption>
+		<thead>
+			<tr>
+				<th>Project Name</th>
+				<th>Project Path</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>{{$project->label}}</td>
+				<td>{{$project->path}}</td>
+				<td>
+					<a href="{{ route('api.filesystem.open', ['path' => $project->path]) }}">Open in explorer</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </x-app-layout>
