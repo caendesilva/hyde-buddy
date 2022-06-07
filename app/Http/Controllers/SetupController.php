26fceca3b98e977b5cb74dfd966d56969873c0dd
Caen De Silva <caen@desilva.se>
2022-06-07 12:32:33 +0200

    Create SetupController.php

---
+++ app/Http/Controllers/SetupController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+
+class SetupController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return view('setup');
+    }
+
+    public function setup()
+    {
+        try {
+            // Run the migrations
+            Artisan::call('migrate', ['--seed' => true]);
+
+            // Test the connection
+            DB::table('migrations')->count();
+
+
+            // Set the initialized state
+            touch(app()->make('homePath') . '\\database\\.initialized');
+        } catch (\Throwable $th) {
+            // Rollback state
+            if (file_exists(app()->make('homePath') . '\\database\\.initialized')) {
+                unlink(app()->make('homePath') . '\\database\\.initialized');
+            }
+
+            throw $th;
+        }
+
+        return redirect()->route('welcome')->with('toast', 'Setup complete!');
+    }
+
+    
+    public static function isSetup()
+    {
+        return file_exists(app()->make('homePath') . '\\database\\.initialized');
+    }
+}
