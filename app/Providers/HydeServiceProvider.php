67c3f9cbb9dc33e325d53ae1a5b88c75bc008d85
Caen De Silva <caen@desilva.se>
2022-06-07 16:06:22 +0200

    Copy over the registration code
    As I don't want to register the commands

--- app/Providers/HydeServiceProvider.php
+++ app/Providers/HydeServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Composer\InstalledVersions;
+use Hyde\Framework\Contracts\AssetServiceContract;
 use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\BladePage;
+use Hyde\Framework\Models\DocumentationPage;
+use Hyde\Framework\Models\MarkdownPage;
+use Hyde\Framework\Models\MarkdownPost;
+use Hyde\Framework\Services\AssetService;
 use Illuminate\Support\ServiceProvider;
 
 class HydeServiceProvider extends \Hyde\Framework\HydeServiceProvider
@@ -15,6 +22,41 @@ class HydeServiceProvider extends \Hyde\Framework\HydeServiceProvider
 
         Hyde::setBasePath(file_get_contents($this->app->make('homePath') . '\\database\\activeProject'));
 
-        parent::register();
+        /**
+         * @deprecated
+         */
+        $this->app->bind(
+            'hyde.version',
+            function () {
+                return InstalledVersions::getPrettyVersion('hyde/hyde') ?: 'unreleased';
+            }
+        );
+
+        /**
+         * @deprecated
+         */
+        $this->app->bind(
+            'framework.version',
+            function () {
+                return InstalledVersions::getPrettyVersion('hyde/framework') ?: 'unreleased';
+            }
+        );
+
+        $this->app->singleton(AssetServiceContract::class, AssetService::class);
+
+        $this->registerDefaultDirectories([
+            BladePage::class => '_pages',
+            MarkdownPage::class => '_pages',
+            MarkdownPost::class => '_posts',
+            DocumentationPage::class => '_docs',
+        ]);
+
+        $this->discoverBladeViewsIn('_pages');
+
+        $this->storeCompiledSiteIn(config(
+            'hyde.site_output_path', Hyde::path('_site')
+        ));
     }
+
+
 }
