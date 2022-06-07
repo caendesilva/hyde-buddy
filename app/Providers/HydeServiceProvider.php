41299d4c8ca9da407517463f72e6e5194c7822c6
Caen De Silva <caen@desilva.se>
2022-06-07 15:38:08 +0200

    Skip registration if application is not setup

--- app/Providers/HydeServiceProvider.php
+++ app/Providers/HydeServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Providers;
 
+use Hyde\Framework\Hyde;
 use Illuminate\Support\ServiceProvider;
 
 class HydeServiceProvider extends \Hyde\Framework\HydeServiceProvider
 {
-    //
+    public function register(): void
+    {
+        if (! $this->app->make('isSetup')) {
+            return;
+        }
+
+        parent::register();
+    }
 }
